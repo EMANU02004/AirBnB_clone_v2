7043b0b94eddfac75df9b860c3cfa221b8a6d6cd
EMANU02004 <masambue45@gmail.com>
2023-11-22 01:01:28 -0800

    models update

--- models/city.py
+++ models/city.py
@@ -9,6 +9,7 @@ from models.base_model import BaseModel, Base
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
     name = Column(
         String(128), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
