e2072363e787f8c70150d6a8b538b569d630e764
Emmanuel Masambu <125499034+EMANU02004@users.noreply.github.com>
2023-12-06 16:21:39 +0000

    bnb update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,15 +7,42 @@ from fabric.api import
 env.hosts = ['54.90.39.8', '35.153.226.155']
 """List of the server IP addresses."""
 
+
+@runs_once
+def do_pack():
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
+
+
 def do_deploy(archive_path):
-    """ Deploys """
+    """Deploys the static files to the host servers.
+    Args:
+        archive_path (str): The path to the archived static files.
+    """
     if not os.path.exists(archive_path):
         return False
     file_name = os.path.basename(archive_path)
     folder_name = file_name.replace(".tgz", "")
     folder_path = "/data/web_static/releases/{}/".format(folder_name)
     success = False
-
     try:
         put(archive_path, "/tmp/{}".format(file_name))
         run("mkdir -p {}".format(folder_path))
